df07b0d91be5b19a41485ccd2d8360d2e600353f
Brendan O'Brien <bobrien@pursuit.org>
2024-07-17 14:42:51 -0400

    creates db schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,15 @@
-kj
+DROP DATABASE IF EXISTS bikes_dev;
+CREATE DATABASE bides_dev;
+
+\c bikes_dev;
+
+CREATE TABLE bikes (
+    id SERIAL PRIMARY KEY,
+    brand TEXT NOT NULL,
+    year INTEGER,
+    price INTEGER,
+    bikeType TEXT,
+    frame TEXT,
+    bikeWeight FLOAT,
+    newBike: boolean
+);
