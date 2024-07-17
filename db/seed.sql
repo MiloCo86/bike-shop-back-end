2da6cf810ed42d54cf23c82c5244f9c0686b69f8
MiloCo86 <cpaez@pursuit.org>
2024-07-17 16:16:01 -0400

    fixing bikeweight to bike_weigth, adding a boolean value to seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -1,3 +1,5 @@
-INSERT INTO bikes (brand, year, price, bike_type, frame, bikeWeight, is_new) VALUES
-('BMX', '1984.000', 400.00, 'normal', 'lightweight', 10.5),
-('BMX', '2011.45', 300.00, 'mountain', 'cross-country', 13.0);
+\c bikes_dev;
+
+INSERT INTO bikes (brand, year, price, bike_type, frame, bike_weight, is_new) VALUES
+('BMX', 1987, 400.00, 'normal', 'lightweight', 10.5, false),
+('BMX', 2023, 300.00, 'mountain', 'cross-country', 13.0, true);
