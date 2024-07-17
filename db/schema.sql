14bb2c28abc86d97ef54faa5feb48a8bd2748642
Brendan O'Brien <bobrien@pursuit.org>
2024-07-17 15:59:16 -0400

    changes boolean to BOOLEAN

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,15 @@
-kj
+DROP DATABASE IF EXISTS bikes_dev;
+CREATE DATABASE bikes_dev;
+
+\c bikes_dev;
+
+CREATE TABLE bikes (
+    id SERIAL PRIMARY KEY,
+    brand TEXT NOT NULL,
+    year DATE,
+    price FLOAT NOT NULL,
+    bike_type TEXT NOT NULL,
+    frame TEXT,
+    bike_weight FLOAT,
+    is_new: boolean NOT NULL
+);
