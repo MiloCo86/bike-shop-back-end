e546ba26ad2c4ab7b1c9c6d99fe0a5401993a6dd
Brendan O'Brien <bobrien@pursuit.org>
2024-07-18 14:28:08 -0400

    changes checkBrand to checkMake in the controller

--- controllers/bikesController.js
+++ controllers/bikesController.js
@@ -7,7 +7,7 @@ const { getAllBikes ,getBike, addBike, deleteBike, updateBike } = require('../db
 
 //import validations
 
-const { checkBrand, checkYear, checkPrice, checkWeight, checkStock } = require('../validations/checkBikes')
+const { checkMake, checkYear, checkPrice, checkWeight, checkStock } = require('../validations/checkBikes')
 
 // Index Routes: gets all bikes
 // localhost:4001/bikes/
@@ -31,7 +31,7 @@ bikes.get('/:id', async (req,res)=>{
   }
 })
 
-bikes.post("/", checkBrand, checkYear, checkPrice, checkWeight, checkStock, async (req, res) => {
+bikes.post("/", checkMake, checkYear, checkPrice, checkWeight, checkStock, async (req, res) => {
   const bike = await addBike(req.body);
   res.json(bike);
 });
