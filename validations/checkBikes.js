181c2a38490edf2b1ddac95a9016cf97da031c8a
Brendan O'Brien <bobrien@pursuit.org>
2024-07-18 14:22:10 -0400

    changes brand to make adds checkStock validation updates controller middleware

--- validations/checkBikes.js
+++ validations/checkBikes.js
@@ -1,6 +1,6 @@
-const checkBrand = (req, res, next) => {
-    const { brand } = req.body;
-        if (brand) {
+const checkMake = (req, res, next) => {
+    const { make } = req.body;
+        if (make) {
             next();
         } else {
             res.status(400).json({ error: "Name of bike manufacturer is required."})
@@ -36,4 +36,13 @@ const checkPrice = (req, res, next) => {
         }
 }
 
-module.exports = { checkBrand, checkYear, checkPrice, checkWeight }
+const checkStock = (req, res, next) => {
+    const { in_stock } = req.body;
+    if (in_stock > 0) {
+        next();
+    } else {
+        res.status(400).json({ error: "Stock must be greater than 0"})
+    }
+}
+
+module.exports = { checkBrand, checkYear, checkPrice, checkWeight, checkStock }
