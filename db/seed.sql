e0dde64177cff14bd56cf08e0afb1cbe7ed35c2d
MiloCo86 <cpaez@pursuit.org>
2024-07-19 00:18:44 -0400

    adding 20 examples to seed file

--- db/seed.sql
+++ db/seed.sql
@@ -1,5 +1,23 @@
 \c bikes_dev;
 
 INSERT INTO bikes (make, model, year, price, bike_type, frame, bike_weight, is_new, in_stock, img_url) VALUES
-('BMX', 'testname', 1987, 400.00, 'normal', 'lightweight', 10.5, false, 11, ''),
-('BMX', 'test2', 2023, 300.00, 'mountain', 'cross-country', 13.0, true, 0, '');
+('Trek', 'FX Sport 4', 2022, 1799.99, 'Urban', 'Carbon', 32, true, 2, 'https://centracycles.com/image/cache/catalog/products/trek-fx-sport-carbon-4-disc-hybrid-bike-2021-dark-aquatic-carbon-smoke-346590-800x800.jpg'),
+('Cannondale', 'Topstone 4', 2022, 1300.00, 'Road', 'Lightweight', 28, true, 1, 'https://www.sefiles.net/images/library/zoom/cannondale-topstone-4-386396-1.png'),
+('Specialized', 'Rockhopper', 2023, 1200.00, 'Mountain', 'Aluminum', 29, true, 3, 'https://www.specialized.com/us/en/rockhopper'),
+('Giant', 'Escape 3', 2022, 450.00, 'Hybrid', 'Aluminum', 26, true, 5, 'https://www.giant-bicycles.com/us/escape-3'),
+('Santa Cruz', 'Chameleon', 2023, 2100.00, 'Mountain', 'Carbon', 27, true, 2, 'https://www.santacruzbicycles.com/en-US/chameleon'),
+('Scott', 'Addict 20', 2022, 2300.00, 'Road', 'Carbon', 19, true, 1, 'https://www.scott-sports.com/us/en/product/scott-addict-20-bike'),
+('Bianchi', 'Aria', 2023, 2700.00, 'Road', 'Carbon', 18, true, 1, 'https://www.bianchi.com/bikes/aria/'),
+('Cube', 'Attain SL', 2022, 1600.00, 'Road', 'Aluminum', 22, true, 4, 'https://www.cube.eu/en/2022/road-race/attain/cube-attain-sl'),
+('Raleigh', 'Detour 2', 2023, 500.00, 'Hybrid', 'Steel', 30, true, 6, 'https://www.raleighusa.com/detour-2'),
+('Norco', 'Fluid FS 3', 2022, 2000.00, 'Mountain', 'Aluminum', 33, true, 2, 'https://www.norco.com/bikes/2022/mountain/trail/fluid-fs/fluid-fs-3'),
+('Kona', 'Rove AL 650', 2022, 999.00, 'Gravel', 'Aluminum', 25, true, 3, 'https://konaworld.com/rove_al_650.cfm'),
+('Merida', 'Silex 400', 2023, 1800.00, 'Gravel', 'Aluminum', 24, true, 2, 'https://www.merida-bikes.com/en/bike/445/silex-400'),
+('GT', 'Grade Expert', 2022, 1900.00, 'Gravel', 'Aluminum', 23, true, 1, 'https://www.gtbicycles.com/usa_en/2022/bikes/grade-expert'),
+('Fuji', 'Jari 2.3', 2023, 1500.00, 'Gravel', 'Steel', 27, true, 3, 'https://www.fujibikes.com/usa/bikes/road/adventure/jari-2-3'),
+('Marin', 'Headlands 1', 2022, 2200.00, 'Gravel', 'Carbon', 22, true, 2, 'https://www.marinbikes.com/bikes/headlands-1'),
+('Surly', 'Ogre', 2023, 1750.00, 'Touring', 'Steel', 30, true, 2, 'https://surlybikes.com/bikes/ogre'),
+('Cannondale', 'Quick CX 3', 2022, 725.00, 'Hybrid', 'Aluminum', 28, true, 4, 'https://www.cannondale.com/en/bikes/fitness-urban/fitness/quick-cx-3'),
+('Electra', 'Townie 7D', 2023, 629.99, 'Cruiser', 'Aluminum', 35, true, 5, 'https://electra.trekbikes.com/us/en_US/townie-7d/p/33181/'),
+('Yeti', 'SB115', 2023, 5400.00, 'Mountain', 'Carbon', 28, true, 1, 'https://www.yeticycles.com/bikes/sb115'),
+('Cervelo', 'Aspero', 2023, 3000.00, 'Gravel', 'Carbon', 20, true, 2, 'https://www.cervelo.com/en-US/aspero');
